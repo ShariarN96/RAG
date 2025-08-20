0fed8e2a98026f9d8899a4160816cc994869475a
Shahriar-Namvar <shahriarnamvar91@gmail.com>
2025-08-19 22:51:38 -0500

    module erro fixed!

--- src/ETL/crossref.py
+++ src/ETL/crossref.py
@@ -3,25 +3,6 @@ import requests
 BASE = "https://api.crossref.org/works"
 
 
-# def wiley_dois_2025_by_keyword(query, limit=5):
-#     base_params = {
-#         "query": query,
-#         "filter": "member:311,type:journal-article,from-pub-date:2025-01-01,until-pub-date:2025-12-31",
-#         "rows": 0,
-#     }
-#     headers = {"User-Agent": "wiley-doi-fetcher/0.1"}
-#     total = requests.get(BASE, params=base_params, headers=headers).json()["message"][
-#         "total-results"
-#     ]
-
-
-#     offset = random.randint(0, max(0, total - limit))
-#     params = base_params | {"rows": limit, "offset": offset, "select": "DOI"}
-#     r = requests.get(BASE, params=params, headers=headers)
-#     items = r.json()["message"]["items"]
-#     return [it["DOI"] for it in items if "DOI" in it]
-
-
 def wiley_dois_2025_by_keyword(query, limit=5):
     """
     Fetch up to `limit` random DOIs from Wiley-Blackwell (member:311), year 2025,
@@ -51,17 +32,17 @@ if __name__ == "__main__":
     for doi in wiley_dois_2025_by_keyword("high-entropy alloy", limit=10):
         print(doi)
 
-# items = wiley_dois_2025_by_keyword("high-entropy alloy", limit=10)
+items = wiley_dois_2025_by_keyword("high-entropy alloy", limit=10)
 
-# x = items[0]
+x = items[0]
 
-# columns = ["DOI", "title", "URL", "container-title", "created"]
+columns = ["DOI", "title", "URL", "container-title", "created"]
 
-# for i in columns:
-#     print(x[i])
+for i in columns:
+    print(x[i])
 
-# x["DOI"]
-# x["title"][0]
-# x["URL"]
-# x["container-title"]
-# x["created"]["date-time"][0:10]
+x["DOI"]
+x["title"][0]
+x["URL"]
+x["container-title"]
+x["created"]["date-time"][0:10]
--- src/data_model/model.py
+++ src/data_model/model.py
@@ -1,18 +1,22 @@
 import os
 
+from pydantic import BaseModel, Field
+
+from src.etl.crossref import print_results
+
 current_dir = os.path.dirname(__file__)
 
-from etl.crossref import print_results
 
-# class ArticleMetadata(BaseModel):
-#     doi: str = Field(description="Digital Object Identifier of the article")
+class ArticleMetadata(BaseModel):
+    doi: str = Field(description="Digital Object Identifier of the article")
 
-#     title: str = Field(description="Title of the article")
+    title: str = Field(description="Title of the article")
 
-#     url: str = Field(description="URL of the article")
+    url: str = Field(description="URL of the article")
 
-#     container_title: str = Field(description="Container title (e.g., journal name)")
+    container_title: str = Field(description="Container title (e.g., journal name)")
 
-#     publication_date: str = Field(description="Publication date in YYYY-MM-DD format")
+    publication_date: str = Field(description="Publication date in YYYY-MM-DD format")
+
 
 print_results()
